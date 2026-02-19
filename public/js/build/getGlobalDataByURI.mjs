635f46fa9f5c56d80b4ab2af6f96f4e42b89b35d
karenrmelikyan <karenrmelikyan@gmail.com>
2026-02-19 21:04:41 +0400

    some structure improvement and added several new pages

--- public/js/build/getGlobalDataByURI.mjs
+++ public/js/build/getGlobalDataByURI.mjs
@@ -1,16 +1,16 @@
 export default function getGlobalDataByURI(URI) {
     const prices = [
-        { id: "lvacqi-meqenaneri-veranorogum", value: "10.000" },
-        { id: "sarnaranneri-veranorogum", value: "10.000" },
-        { id: "spasq-lvacox-meqenaneri-veranorogum", value: "15.000" },
-        { id: "avtomat-choranocneri-veranorogum", value: "15.000" }
+        { id: "/lvacqi-meqenaneri-veranorogum/", value: "10.000" },
+        { id: "/sarnaranneri-veranorogum/", value: "10.000" },
+        { id: "/spasq-lvacox-meqenaneri-veranorogum/", value: "15.000" },
+        { id: "/avtomat-choranocneri-veranorogum/", value: "15.000" },
     ]
 
     const sources = [
-        { id: "lvacqi-meqenaneri-veranorogum", value: "/assets/img/washer-service.webp" },
-        { id: "sarnaranneri-veranorogum", value: "/assets/img/fridge-service.webp" },
-        { id: "spasq-lvacox-meqenaneri-veranorogum", value: "/assets/img/dishwasher-service.webp" },
-        { id: "avtomat-choranocneri-veranorogum", value: "/assets/img/dryer-service.webp" }
+        { id: "/lvacqi-meqenaneri-veranorogum/", value: "/assets/img/washer-service.webp" },
+        { id: "/sarnaranneri-veranorogum/", value: "/assets/img/fridge-service.webp" },
+        { id: "/spasq-lvacox-meqenaneri-veranorogum/", value: "/assets/img/dishwasher-service.webp" },
+        { id: "/avtomat-choranocneri-veranorogum/", value: "/assets/img/dryer-service.webp" },
     ]
     
     const priceData = prices.find(item => URI.includes(item.id))
