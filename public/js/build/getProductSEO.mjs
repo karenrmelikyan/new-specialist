adaf70f7d8d1267eba9fbe2081e42b1dfcbcebf9
karenrmelikyan <karenrmelikyan@gmail.com>
2026-02-28 21:13:59 +0400

    completed russian content

--- public/js/build/getProductSEO.mjs
+++ public/js/build/getProductSEO.mjs
@@ -1,13 +1,30 @@
 export default function getProductSEO(title, description, localization = 'hy') {
     if (localization === 'ru') {
         return {
-            title: `${title} | Specialist Ереван`,
+            title: getStrWithoutDuplicates(`${title} ремонт в Ереване | Specialist`),
             description: `${description} - закажите ремонт в Ереване. Сервисная служба с рейтингом ⭐4.9/5. Гарантия, оригинальные запасные части, высокое качество облсуживания и быстрота отклика.`,
         }
     } else {
         return {
-            title: `${title} | Specialist Երևան`,
+            title: getStrWithoutDuplicates(`${title} վերանորոգում Երևանում | Specialist`),
             description: `${description} - պատվիրեք վերանորոգում Երևանում: Կենցաղային ծառայություն ⭐4.9/5 վարկանիշով: Երաշխիք, օրիգինալ պահեստամասեր, բարձրորակ սպասարկում և արագ արձագանք։`,
         }
     }
+}
+
+function getStrWithoutDuplicates(str) {
+    const words = str.split(' ')
+    const seenWords = new Set()
+    const result = []
+
+    for (const word of words) {
+        const lowerWord = word.toLowerCase()
+
+        if (!seenWords.has(lowerWord)) {
+            seenWords.add(lowerWord)
+            result.push(word)
+        }
+    }
+
+    return result.join(' ')
 }
